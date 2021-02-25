28f2da8a70bd9d01173731b5438fe8cb144e3f25
Olivier71 <olivier.nesme@gmail.com>
2021-02-25 09:55:26 +0100

    Add accounts path to urls

--- broadcast_alert/urls.py
+++ broadcast_alert/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("programmes.urls")),
+    path('', include("accounts.urls")),
     path('admin/', admin.site.urls),
 ]
