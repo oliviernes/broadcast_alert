86250280c2c6b9fc774fcae5d5fb0c7cf74c43f5
Olivier71 <olivier.nesme@gmail.com>
2021-02-25 09:56:22 +0100

    Add my_search path to urls path

--- programmes/urls.py
+++ programmes/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.search, name="welcome"),
     path('results/', views.search, name="results"),
+    path('my_search/', views.my_search, name="my_search"),
 ]
