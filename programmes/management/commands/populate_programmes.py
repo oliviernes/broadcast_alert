8e45b5966cd4f8ae31a3186567841ca84d7ffd7e
Olivier71 <olivier.nesme@gmail.com>
2021-03-02 06:36:11 +0100

    Remove useless import in populate_programmes.py

--- programmes/management/commands/populate_programmes.py
+++ programmes/management/commands/populate_programmes.py
@@ -3,7 +3,6 @@ import datetime
 import json
 import os
 
-from django.conf import settings
 from django.utils.timezone import make_aware
 from django.core.management.base import BaseCommand
 from django.db import transaction
