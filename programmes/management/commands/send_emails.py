e3e0f89d75b129e784970fb840478e4a590139fe
Olivier71 <olivier.nesme@gmail.com>
2021-03-03 21:57:33 +0100

    Add send emails command

--- programmes/management/commands/send_emails.py
+++ programmes/management/commands/send_emails.py
@@ -5,8 +5,12 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.utils.timezone import make_aware
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 from programmes.models import Programmes, Recherche, Chaines
+
+import pdb
 
 class Command(BaseCommand):
     help = "Search programmes in the database meeting requirements"
@@ -14,29 +18,63 @@ class Command(BaseCommand):
     def send_email(self):
 
         users = User.objects.all()
+        # users = User.objects.all()[:1]
 
         for user in users:
 
             searches = Recherche.objects.filter(utilisateur_id=user.id)
 
+            infos_mail = []
+            email_to_send = False
+
             for search in searches:
                 prog_match = search.programmes.all()
                 new_prog = []
-                week_prog = []
+                old_prog = []
                 if len(prog_match) > 0:
                     for prog in prog_match:
-                        # if prog.date_debut > make_aware(datetime.datetime.now() + timedelta(6)):
-                        if prog.date_debut > make_aware(datetime.datetime.now() + timedelta(1)):                        
-                            new_prog.append(prog)
-                        elif make_aware(datetime.datetime.now() < prog.date_debut < make_aware(datetime.datetime.now() + timedelta(6))):
-                            week_prog.append(prog)
+                        titres = prog.titres_set.all()
+                        if len(titres) > 0:
+                            titre = titres[0].nom
+                            # if prog.date_debut > make_aware(datetime.datetime.now() + timedelta(6)):
+                            if prog.date_debut > make_aware(datetime.datetime.now() + timedelta(5)):
+                                new_prog.append({'prog': prog,
+                                                'chaine': Chaines.objects.get(id=prog.chaines_id),
+                                                'titre': titre
+                                                })
+                                email_to_send = True
+                            if make_aware(datetime.datetime.now()) < prog.date_debut < make_aware(datetime.datetime.now()) + timedelta(5):
+                                old_prog.append({'prog': prog,
+                                                'chaine': Chaines.objects.get(id=prog.chaines_id),
+                                                'titre': titre
+                                })
                 # if search.date_creation < make_aware(datetime.datetime.now() - timedelta(7)):
                 if search.date_creation < make_aware(datetime.datetime.now()):
-                    if len(new_prog) == 1:
-                        chaine = Chaines.objects.get(id=new_prog[0].chaines_id)
+                    infos_search = { "search": search,
+                        'new_prog': new_prog,
+                        'old_prog': old_prog
+                    }
+                    infos_mail.append(infos_search)
+
+            if email_to_send:
+                if len(infos_mail) == 1:
+                    if len(infos_mail[0]['new_prog']) == 1:
                         subject = "Un programme correspond à votre recherche!"
-                        message = f"Votre recherche créée le {search.date_creation} a obtenu un résultat: {new_prog[0].titres_set.all()[0]}, le {new_prog[0].date_debut} sur la chaîne {chaine.nom}"
-                        send_mail(subject, message, 'popular.crazy2@gmail.com', [user.email])
+                    elif len(infos_mail[0]['new_prog']) > 1:
+                        subject = "Des programmes correspondent à votre recherche!"
+                elif len(infos_mail) > 1:
+                    subject = "Des programmes correspondent à vos recherches!"
+
+                context = { 'infos_mail': infos_mail }
+
+                from_email, to = 'popular.crazy2@gmail.com', user.email,
+
+                text_content = render_to_string('programmes/email.txt', context)
+                html_content = render_to_string('programmes/email.html', context)
+                msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+                msg.attach_alternative(html_content, "text/html")
+                msg.send()
+
 
     def handle(self, *args, **options):
         
