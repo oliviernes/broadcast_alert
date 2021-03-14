3802d28450b7458b725330754cafeb81d554a3f8
Olivier71 <olivier.nesme@gmail.com>
2021-03-14 21:08:53 +0100

    Set a MaxValueValidator to max_resultats

--- programmes/models.py
+++ programmes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -98,7 +99,7 @@ class Compositeurs(models.Model):
 
 class Recherche(models.Model):
     recherche = models.CharField(max_length=200, null=True, blank=True)
-    max_resultats = models.SmallIntegerField()
+    max_resultats = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(20)])
     programmes = models.ManyToManyField(Programmes)
     chaines = models.ManyToManyField(Chaines)
     date_creation = models.DateTimeField(auto_now=True)
