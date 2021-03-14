fc42fc987c81a5597fe4ccf1eccf5f664f16d8e9
Olivier71 <olivier.nesme@gmail.com>
2021-03-14 15:18:25 +0100

    Remove useless results url

--- programmes/urls.py
+++ programmes/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.search, name="welcome"),
-    path("results/", views.search, name="results"),
     path("my_results/<int:my_search_id>", views.my_results, name="my_results"),
     path("my_search/", views.my_search, name="my_search"),
     path('delete/<int:pk>', views.delete, name="delete"),
